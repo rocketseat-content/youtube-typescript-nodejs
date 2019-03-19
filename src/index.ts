e1c0368ff398540406ce737fa220348407f9f0e7
Diego Fernandes <diego.schell.f@gmail.com>
2019-03-19 10:11:26 -0300

    Estrutura Node/Sucrase/ESLint/Prettier

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+import routes from './routes'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(3333)
